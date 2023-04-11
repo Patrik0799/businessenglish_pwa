91099eded9b117c384c150793cabf0a36056f7c1
Patrik <pzelen00@fesb.hr>
2023-04-11 14:03:26 +0200

    Initial commit

---
+++ components/ccm.app_collection.js
@@ -0,0 +1,164 @@
+/**
+ * @overview ccmjs-based web component for an app collection
+ * @author Tea Kless <tea.kless@web.de> 2023
+ * @author André Kless <andre.kless@web.de> 2022
+ * @license The MIT License (MIT)
+ * @version latest (3.0.0)
+ * @changes
+ * version 3.0.0 (06.02.2023):
+ * - added burger menu
+ * - fixed header and footer with scrollable content
+ * version 2.5.1 (30.11.2022):
+ * - uses ccmjs v27.4.2 as default
+ * - uses helper.mjs v8.4.2 as default
+ * version 2.5.0 (26.09.2022):
+ * - an entry in the app collection can lead to an external URL
+ * - an entry in the app collection can initiate writing to an email address
+ * - disabled dark mode as default
+ * version 2.4.0 (07.03.2022):
+ * - individual section color
+ * version 2.3.0 (25.02.2022):
+ * - uses ccmjs v27.3.1 as default
+ * - uses helper.mjs v8.1.0 as default
+ * - apps in the app collection can also be given by app url or embed code
+ * version 2.2.0 (11.02.2022): controllable dark mode
+ * version 2.1.0 (08.02.2022): content of clicked entry can contain multiple apps
+ * version 2.0.0 (24.01.2022): reimplementation by akless
+ */
+
+( () => {
+  const component = {
+    name: 'app_collection',
+    ccm: 'https://ccmjs.github.io/ccm/versions/ccm-27.4.2.min.js',
+    config: {
+//    "color": "limegreen",
+      "css": [ "ccm.load", "https://ccmjs.github.io/tkless-components/app_collection/resources/styles-3.0.0.min.css" ],
+//    "dark": "auto",
+      "footer": [],
+      "helper": [ "ccm.load", "https://ccmjs.github.io/akless-components/modules/versions/helper-8.4.2.min.mjs" ],
+      "html": [ "ccm.load", "https://ccmjs.github.io/tkless-components/app_collection/resources/templates-3.0.0.min.mjs" ],
+//    "icon": [ "ccm.load", "https://ccmjs.github.io/tkless-components/app_collection/resources/app.svg" ],
+//    "lang": [ "ccm.start", "https://ccmjs.github.io/akless-components/lang/versions/ccm.lang-1.1.0.min.js" ],
+//    "logger": [ "ccm.instance", "https://ccmjs.github.io/akless-components/log/versions/ccm.log-5.0.1.min.js", [ "ccm.get", "https://ccmjs.github.io/akless-components/log/resources/configs.min.js", "greedy" ] ],
+//    "routing": [ "ccm.instance", "https://ccmjs.github.io/akless-components/routing/versions/ccm.routing-3.0.0.min.js" ],
+      "sections": [],
+//    "title": "",
+//    "user": [ "ccm.start", "https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.7.2.min.js" ]
+    },
+    Instance: function () {
+
+      /**
+       * shortcut to help functions
+       * @type {Object.<string,Function>}
+       */
+      let $;
+
+      /**
+       * when all dependencies are solved after creation and before the app starts
+       * @returns {Promise<void>}
+       */
+      this.ready = async () => {
+
+        // set shortcut to help functions
+        $ = Object.assign( {}, this.ccm.helper, this.helper ); $.use( this.ccm );
+
+        // define routes
+        this.routing && this.routing.define( { home: ( i, j ) => render( parseInt( i ), parseInt( j ) ) } );
+
+        // setup dark mode
+        this.dark === 'auto' && this.element.classList.add( 'dark_auto' );
+        this.dark === true && this.element.classList.add( 'dark_mode' );
+
+        // set individual section color
+        this.color && this.element.style.setProperty( '--section-bg', this.color );
+
+        // logging of 'ready' event
+        this.logger && this.logger.log( 'ready', $.privatize( this, true ) );
+
+      };
+
+      /**
+       * starts the app
+       * @returns {Promise<void>}
+       */
+      this.start = async () => {
+
+        // logging of 'start' event
+        this.logger && this.logger.log( 'start' );
+
+        // render main HTML template
+        this.html.render( this.html.main( this, onClick, navBars ), this.element );
+
+        // render content
+        if ( this.routing && this.routing.get() )
+          await this.routing.refresh();
+        else
+          await render();
+
+        // translate content and render language selection
+        if ( this.lang ) {
+          this.lang.translate();
+          !this.lang.getContext() && $.append( this.element.querySelector( '#nav' ), this.lang.root );
+        }
+
+        // render user login/logout button
+        this.user && $.append( this.element.querySelector( '#nav' ), this.user.root );
+
+      };
+
+      /**
+       * when an entry is clicked
+       * @param {number} [i] - number of clicked footer entry or section (1,..,n)
+       * @param {number} [j] - number of clicked entry in the section (1,..,m)
+       * @returns {Promise<void>}
+       */
+      const onClick = async ( i, j ) => {
+        const entry = i && ( !j ? this.footer[ i - 1 ] : this.sections[ i - 1 ].entries[ j - 1 ] );
+        if ( entry && !entry.ignore ) return;
+        if ( entry && typeof entry.ignore === 'string' && entry.ignore.startsWith( 'http' ) && !( await $.appDependency( entry.ignore ) ) )
+          window.open( entry.ignore );
+        else if ( entry && typeof entry.ignore === 'string' && entry.ignore.startsWith( 'mailto' ) )
+          window.location.href = entry.ignore;
+        else {
+          this.routing && this.routing.set( 'home' + ( i ? '-' + i + ( j ? '-' + j : '' ) : '' ) );
+          await render( i, j );
+        }
+      };
+
+      /**
+       * renders home or content of a clicked entry
+       * @param {number} [i] - number of clicked footer entry or section (1,..,n)
+       * @param {number} [j] - number of clicked entry in the section (1,..,m)
+       * @returns {Promise<void>}
+       */
+      const render = async ( i, j ) => {
+        let element = this.element.querySelector( 'main' );
+        $.setContent( element, $.loading() );
+        this.html.render( this.html.headline( this.title, i && ( this[ j ? 'sections' : 'footer' ][ i - 1 ].title ), j && this.sections[ i - 1 ].entries[ j - 1 ].title ), this.element.querySelector( '#headline' ) );
+
+        // render home
+        if ( !i ) {
+          $.replace( element, element = element.cloneNode() );            // resets lit-html template
+          this.html.render( this.html.home( this, onClick ), element );
+        }
+        // render content of clicked entry
+        else {
+          const entry = !j ? this.footer[ i - 1 ] : this.sections[ i - 1 ].entries[ j - 1 ];
+          if ( !Array.isArray( entry.ignore ) || $.isDependency( entry.ignore ) ) entry.ignore = [ entry.ignore ];
+          entry.ignore = await Promise.all( entry.ignore.map( app => $.isInstance( app ) ? app : $.appDependency( app ).then( app => $.solveDependency( app, this ) ) ) );
+          element.innerHTML = '';
+          entry.ignore.forEach( content => $.append( element, $.isInstance( content ) ? content.root : $.html( content ) ) );
+        }
+
+      };
+
+      const navBars = () => {
+        this.element.querySelector( '.bars' ).classList.toggle( "change" );
+        const elem = this.element.querySelector( "#myLinks" );
+        elem.classList.toggle( "visible" );
+      }
+
+    }
+  };
+  let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||[""])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){(c="latest"?window.ccm:window.ccm[c]).component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
---
+++ demo_resources/resources.js
@@ -0,0 +1,109 @@
+ccm.files[ 'resources.js' ] = {
+    "presentsimple" : {
+        "cancel_button": "",
+        "captions": {
+            "retry": "Retry",
+            "submit": "Check",
+            "start": "Start",
+            "finish": "Finish"
+        },
+        "css": [
+            "ccm.load",
+            "https://ccmjs.github.io/akless-components/cloze/resources/pbworks.css"
+        ],
+        "helper": [
+            "ccm.load",
+            "https://ccmjs.github.io/akless-components/modules/versions/helper-5.1.0.mjs"
+        ],
+        "html": [
+            "ccm.load",
+            "https://ccmjs.github.io/akless-components/cloze/resources/templates.html"
+        ],
+        "logger": "",
+        "oncancel": "",
+        "onchange": "",
+        "onfeedback": "",
+        "oninput": "",
+        "onstart": "",
+        "onvalidation": "",
+        "show_results": "",
+        "blank": true,
+        "keywords": false,
+        "feedback": true,
+        "retry": true,
+        "solutions": false,
+        "progress_bar": true,
+        "time": "",
+        "mark": "*",
+        "data": {
+            "store": [
+                "ccm.store"
+            ],
+            "login": true,
+            "user": true,
+            "key": "1588892835142X7802285741412942"
+        },
+        "user": null,
+        "onfinish": {
+            "restart": true,
+            "confirm": "",
+            "clear": false,
+            "alert": ""
+        },
+        "text": "<p><strong style=\"color: rgb(0, 0, 0);\">Section I - Introductions and the Present Simple</strong></p><p><span style=\"color: rgb(0, 0, 0);\">Being able to introduce yourself well is a key skill in business. In fact, how you introduce yourself will influence how others think of and interact with you. To introduce yourself in English you use the present tenses. These tenses are the present simple and the present progressive.&nbsp;</span></p><p><strong style=\"color: rgb(0, 0, 0);\">Most often the present simple (also called simple present) is used to introduce yourself and to talk about what you do</strong><em style=\"color: rgb(0, 0, 0);\">. </em></p><p><em style=\"color: rgb(0, 0, 0);\"><span class=\"ql-cursor\">﻿﻿﻿﻿﻿﻿﻿﻿</span></em><span style=\"color: rgb(0, 0, 0);\">This is because the present simple is used to talk about permanent states and facts, such as what your name is, where you live or where are from, plus what you do for work, e.g.,</span><em style=\"color: rgb(0, 0, 0);\"> My name is John. I am a systems analyst. I live in Sankt Augustin, Germany</em><span style=\"color: rgb(0, 0, 0);\">.&nbsp;The present simple is also used to talk about thoughts, feelings, habits, e.g., </span><em style=\"color: rgb(0, 0, 0);\">I am happy to meet you in person. I feel a little tired. I drink coffee. We speak English and German at the office.</em></p><h4><strong style=\"color: rgb(0, 0, 0);\">Fill in the blank with the correct form of the words from the box above.</strong></h4><ol><li>My name *is* Judith.</li><li><span style=\"color: rgb(0, 0, 0);\">I *am* called Judy.</span></li><li><span style=\"color: rgb(0, 0, 0);\">I *work* for Ford.</span></li><li><span style=\"color: rgb(0, 0, 0);\">I *live* in Germany.</span></li><li><span style=\"color: rgb(0, 0, 0);\">We *work* together.</span></li><li><span style=\"color: rgb(0, 0, 0);\">The company *produces*</span><span style=\"color: rgb(83, 129, 53);\"> </span><span style=\"color: rgb(0, 0, 0);\">computers.</span></li><li><span style=\"color: rgb(0, 0, 0);\">The firm *is* in London.</span></li><li><span style=\"color: rgb(0, 0, 0);\">I *like* my work.</span></li><li><span style=\"color: rgb(0, 0, 0);\">I *go* to work by train.</span></li><li><span style=\"color: rgb(0, 0, 0);\">She *drinks* tea, not coffee.</span></li><li><span style=\"color: rgb(0, 0, 0);\">I *speak*</span><span style=\"color: rgb(83, 129, 53);\"> </span><span style=\"color: rgb(0, 0, 0);\">English well.</span></li><li><span style=\"color: rgb(0, 0, 0);\">He *does* accounting.</span></li></ol>"
+    },
+
+    "presentprogressive" : {
+        "cancel_button": "",
+        "captions": {
+            "retry": "Retry",
+            "submit": "Check",
+            "start": "Start",
+            "finish": "Finish"
+        },
+        "css": [
+            "ccm.load",
+            "https://ccmjs.github.io/akless-components/cloze/resources/pbworks.css"
+        ],
+        "helper": [
+            "ccm.load",
+            "https://ccmjs.github.io/akless-components/modules/versions/helper-5.1.0.mjs"
+        ],
+        "html": [
+            "ccm.load",
+            "https://ccmjs.github.io/akless-components/cloze/resources/templates.html"
+        ],
+        "logger": "",
+        "oncancel": "",
+        "onchange": "",
+        "onfeedback": "",
+        "oninput": "",
+        "onstart": "",
+        "onvalidation": "",
+        "show_results": "",
+        "blank": true,
+        "keywords": false,
+        "feedback": true,
+        "retry": true,
+        "solutions": false,
+        "progress_bar": true,
+        "time": "",
+        "mark": "*",
+        "data": {
+            "store": [
+                "ccm.store"
+            ],
+            "login": true,
+            "user": true,
+            "key": "1588893711178X049642192995768"
+        },
+        "user": null,
+        "onfinish": {
+            "restart": true,
+            "confirm": "",
+            "clear": false,
+            "alert": ""
+        },
+        "text": "<h4><strong style=\"color: rgb(0, 0, 0);\">Section II - Introductions using Present Progressive to talk about things in progress</strong></h4><p><span style=\"color: rgb(0, 0, 0);\">The present progressive (also called present continuous) is used to talk about: what you are doing at the moment, something happening at the time of speaking (even elsewhere), or about something that is a temporary situation</span><em style=\"color: rgb(0, 0, 0);\">.</em></p><p><span style=\"color: rgb(0, 0, 0);\">This is because the progressive or continuous form always signals actions that are part of a process. This process can be short or long and can even have pauses in between, but it is not finished.&nbsp;</span></p><p><span style=\"color: rgb(0, 0, 0);\">The present progressive always uses a form of the verb “be” plus the main verb with an -ing ending. For example, </span><em style=\"color: rgb(0, 0, 0);\">“I am working at the company’s headquarters this week.”</em><span style=\"color: rgb(0, 0, 0);\">  Notice, this situation is a temporary process lasting just one week. In fact, the present continuous often has “signal words,” such as: this week, today, right now, for the time, and other similar phrases. </span></p><h4><strong style=\"color: rgb(0, 0, 0);\">Fill in the correct form of the word in brackets.</strong></h4><ol><li>We *are* *attending* (attend)<span style=\"color: rgb(83, 129, 53);\"> </span>the conference until Friday.</li><li>We *are* *staying* (stay)<span style=\"color: rgb(83, 129, 53);\"> </span>at the Hilton hotel this week.</li><li>She *is* *filling-in/filling in* (fill-in)<span style=\"color: rgb(83, 129, 53);\"> </span>while I am away.</li><li>We *are*<span style=\"color: rgb(83, 129, 53);\"> </span>*talking*<span style=\"color: rgb(83, 129, 53);\"> </span>(talk)<span style=\"color: rgb(83, 129, 53);\"> </span>about warehouse management.</li><li>Sorry, I *am*<span style=\"color: rgb(83, 129, 53);\"> </span>*having*<span style=\"color: rgb(83, 129, 53);\"> </span>(have)<span style=\"color: rgb(83, 129, 53);\"> </span>problems hearing you.</li><li>I *am*<span style=\"color: rgb(83, 129, 53);\"> </span>*working* (work)<span style=\"color: rgb(83, 129, 53);\"> </span>on the outsourcing project, right now.</li><li>We *are*<span style=\"color: rgb(83, 129, 53);\"> </span>*going*<span style=\"color: rgb(83, 129, 53);\"> </span>(go)<span style=\"color: rgb(83, 129, 53);\"> </span>to a restaurant. Do you want to join us?</li><li>Sorry, I *am*<span style=\"color: rgb(83, 129, 53);\"> </span>*having*<span style=\"color: rgb(83, 129, 53);\"> </span>(have)<span style=\"color: rgb(83, 129, 53);\"> </span>problems understanding you.</li><li>What *are* you *doing*<span style=\"color: rgb(83, 129, 53);\"> </span>(do) in Hamburg?</li><li><span style=\"color: rgb(0, 0, 0);\">Actually, I *am*</span><span style=\"color: rgb(83, 129, 53);\"> </span>*consulting*<span style=\"color: rgb(83, 129, 53);\"> </span>(consult) <span style=\"color: rgb(0, 0, 0);\">with a client in town. </span></li></ol>"
+    }
+}
